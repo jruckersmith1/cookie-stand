025a3f4684f26cc83a1d5329a7cd41b02f784ebe
Janet Rucker-Smith <jruckersmith1@yahoo.com>
2017-08-07 13:57:24 -0700

    1st cookie commit

---
+++ salmon cookie model.js
@@ -0,0 +1,33 @@
+var pdxAirport = {
+minCost: 23,
+maxCost: 65,
+AvgCookieSales: 6.3,
+
+}
+
+var pioneerSquare = {
+minCost: 3,
+maxCost: 24,
+AvgCookieSales: 1.2,
+
+}
+var powells = {
+minCost: 11,
+maxCost: 38,
+AvgCookieSales: 3.7,
+
+}
+
+var stJohns = {
+minCost: 20,
+maxCost: 38,
+AvgCookieSales: 2.3,
+
+}
+var waterfront = {
+minCost: 2,
+maxCost: 16,
+AvgCookieSales: 4.6,
+
+}
+
