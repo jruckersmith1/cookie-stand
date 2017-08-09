a2cc036dd8e3a0985905a68b6a79b0f8cad33a96
Janet Rucker-Smith <jruckersmith1@yahoo.com>
2017-08-09 14:26:07 -0700

    added table and css

--- salmon-cookie.js
+++ salmon-cookie.js
@@ -1,9 +1,11 @@
-function Store ( minCust, maxCust, avgCookieSales, id ) {
 
+function Store ( name, minCust, maxCust, avgCookieSales, id ) {
+  this.name = name;
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.avgCookieSales = avgCookieSales;
-  this.openHours = [ '6am:', '7am', '8am:', '9am:', '10am:', '11am:', '12pm', '1pm:', '2pm:', '3pm:', '4pm:', '5pm:', '6pm:', '7pm:', '8pm:' ];
+  this.avgHour = [];
+  // this.openHours = [ '6am:', '7am', '8am:', '9am:', '10am:', '11am:', '12pm', '1pm:', '2pm:', '3pm:', '4pm:', '5pm:', '6pm:', '7pm:', '8pm:' ];
   this.id = id;
   this.customersPerHour();
   this.addToDom();
@@ -11,37 +13,42 @@ function Store ( minCust, maxCust, avgCookieSales, id ) {
 
 
 Store.prototype.customersPerHour = function () {
-    var avgHour = [];
+  
       for (var i = 0; i < 15; i++) {
       var avgNumber = Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
       avgNumber = Math.floor(avgNumber * this.avgCookieSales);
-      avgHour.push(avgNumber);
+      this.avgHour.push(avgNumber);
  }
-    console.log(avgHour);
-this.cookiesPerHour = avgHour;
+    // console.log(this.avgHour);
+this.cookiesPerHour = this.avgHour;
 }
 
 
 Store.prototype.addToDom = function () {
   console.log(this.id);
-   var displayNum = document.getElementById( this.id );
-    for (var i = 0; i < this.openHours.length; i++) {
+   var displayNum = document.getElementsByTagName( 'tbody' )[0];
+   var newRow = document.createElement ( 'tr' );
+   var storeName = document.createElement ( 'td' );
+   storeName.innerHTML = this.name;
+   newRow.appendChild(storeName);
+   displayNum.appendChild(newRow);
+    for (var i = 0; i < this.avgHour.length; i++) {
       var item = document.createElement ( 'td' );
-    item.innerHTML = this.openHours[i];
-    displayNum.appendChild(item);
-    console.log(this.cookiesPerHour);
-    item.innerHTML = this.openHours[i] + this.cookiesPerHour[i];
+    item.innerHTML = this.avgHour[i];
+    newRow.appendChild(item);
+    // console.log(this.cookiesPerHour);
+    // item.innerHTML = this.openHours[i] + this.cookiesPerHour[i];
 }
-}
+}  
+var pdxAirport = new Store( 'PDX Airport', 23, 65, 6.3, 'pdxAirport' );
 
-var pdxAirport = new Store( 23, 65, 6.3, 'pdxAirport' );
 // pdxAirport.customersPerHour();
 // pdxAirport.addToDom();
 
-var pioneerSquare = new Store( 3, 24, 1.2, 'pioneerSquare' );
-var powells = new Store( 11, 38, 3.7, 'powells' );
-var stJohns = new Store( 20, 38, 2.3, 'stJohns' );
-var waterfront = new Store( 2, 16, 4.6, 'waterfront');
+var pioneerSquare = new Store( 'Pioneer Square', 3, 24, 1.2, 'pioneerSquare' );
+var powells = new Store( 'Powell\'s', 11, 38, 3.7, 'powells' );
+var stJohns = new Store( 'St. John\'s', 20, 38, 2.3, 'stJohns' );
+var waterfront = new Store( 'Waterfront', 2, 16, 4.6, 'waterfront');
 
 
 
